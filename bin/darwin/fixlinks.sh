2cfe5fbe2671fed932443e08a4fd99076f536dcf
Gregg Reynolds <reynolds-gregg@norc.org>
2016-06-23 11:27:10 -0500

    relativize bin/darwin/fixlinks to IOTIVITY_HOME
    Signed-off-by: Gregg Reynolds <reynolds-gregg@norc.org>

--- bin/darwin/fixlinks.sh
+++ bin/darwin/fixlinks.sh
@@ -2,27 +2,40 @@
 
 # fixup dynamic links. maybe not needed if DYLD_LIBRARY_PATH correct?
 
-install_name_tool \
-    -change \
-    out/darwin/x86_64/release/resource/oc_logger/liboc_logger.dylib \
-    /Users/gar/iotivity/iotivation/libiotivity/out/darwin/x86_64/release/resource/oc_logger/liboc_logger.dylib \
-    /Users/gar/iotivity/iotivation/libiotivity/out/darwin/x86_64/release/libocstack-jni.jnilib
+JLP=${IOTIVITY_HOME}/out/darwin/x86_64/release
 
+if [ -d "$IOTIVITY_HOME" ];
+then
+        if [ ! -d "$JLP" ]
+	then
+	        echo "java.library.path not found: $JLP";
+	        exit
+	fi
+else
+	echo "IOTIVITY_HOME ($IOTIVITY_HOME) not found."
+	exit
+fi
 
 install_name_tool \
     -change \
     out/darwin/x86_64/release/resource/src/liboc.dylib \
-    /Users/gar/iotivity/iotivation/libiotivity/out/darwin/x86_64/release/liboc.dylib \
-    /Users/gar/iotivity/iotivation/libiotivity/out/darwin/x86_64/release/libocstack-jni.jnilib
-
-install_name_tool \
-    -change \
-    out/darwin/x86_64/release/resource/csdk/liboctbstack.dylib \
-    /Users/gar/iotivity/iotivation/libiotivity/out/darwin/x86_64/release/liboctbstack.dylib \
-    /Users/gar/iotivity/iotivation/libiotivity/out/darwin/x86_64/release/liboc.dylib
+    $JLP/liboc.dylib \
+    $JLP/libocstack-jni.jnilib
 
 install_name_tool \
     -change \
     out/darwin/x86_64/release/resource/oc_logger/liboc_logger.dylib \
-    /Users/gar/iotivity/iotivation/libiotivity/out/darwin/x86_64/release/resource/oc_logger/liboc_logger.dylib \
-    /Users/gar/iotivity/iotivation/libiotivity/out/darwin/x86_64/release/liboc.dylib
+    $JLP/resource/oc_logger/liboc_logger.dylib \
+    $JLP/libocstack-jni.jnilib
+
+install_name_tool \
+    -change \
+    out/darwin/x86_64/release/resource/csdk/liboctbstack.dylib \
+    $JLP/liboctbstack.dylib \
+    $JLP/libocstack-jni.jnilib
+
+install_name_tool \
+    -change \
+    out/darwin/x86_64/release/resource/oc_logger/liboc_logger.dylib \
+    $JLP/resource/oc_logger/liboc_logger.dylib \
+    $JLP/liboc.dylib
