689f12560985a92e3850e951862d1a60889bae9d
Gregg Reynolds <reynolds-gregg@norc.org>
2016-08-27 12:07:51 -0500

    elim src/include dirs
    using src and include dirs complicates build code but offers no benefit
    in return.
    
    Signed-off-by: Gregg Reynolds <reynolds-gregg@norc.org>

--- resource/c_common/ocrandom.c
+++ resource/c_common/ocrandom.c
@@ -31,15 +31,12 @@
 #endif
 
 #include "platform_features.h"
-#ifdef HAVE_FCNTL_H
+
 #include <fcntl.h>
-#endif
-#ifdef HAVE_UNISTD_H
 #include <unistd.h>
-#endif
-#ifdef HAVE_STDLIB_H
 #include <stdlib.h>
-#endif
+#include <stdio.h>
+
 #ifdef HAVE_SYS_TIME_H
 #include <sys/time.h>
 #endif
@@ -53,12 +50,8 @@
 #ifdef HAVE_WINDOWS_H
 #include <windows.h>
 #endif
-#include "ocrandom.h"
-#include <stdio.h>
 
-#ifdef HAVE_UUID_UUID_H
-#include <uuid/uuid.h>
-#endif
+#include "ocrandom.h"
 
 #define NANO_SEC 1000000000
 
@@ -112,8 +105,13 @@ uint8_t GetRandomBit()
 }
 #endif
 
+// this is only used once, in rsource/connectivity/src/caconnectivitymanager.c
 int8_t OCSeedRandom()
 {
+#ifdef HAVE_ARC4RANDOM_STIR_FN
+    arc4random_stir();
+#else
+
 #ifndef ARDUINO
     // Get current time to Seed.
     uint64_t currentTime = 0;
@@ -131,7 +129,7 @@ int8_t OCSeedRandom()
     gettimeofday(&tv, NULL);
     currentTime = tv.tv_sec * (uint64_t)1000000 + tv.tv_usec;
 #endif
-#if defined(__unix__) || defined(__APPLE__)
+#if defined(__unix__)
     int32_t fd = open("/dev/urandom", O_RDONLY);
     if (fd >= 0)
     {
@@ -177,6 +175,9 @@ int8_t OCSeedRandom()
 
 void OCFillRandomMem(uint8_t * location, uint16_t len)
 {
+#ifdef HAVE_ARC4RANDOM_BUF_FN
+    arc4random_buf((void*)location, (size_t)len);
+#else
     if (!location)
     {
         return;
@@ -185,18 +186,25 @@ void OCFillRandomMem(uint8_t * location, uint16_t len)
     {
         *location++ = OCGetRandomByte();
     }
+#endif
 }
 
 uint32_t OCGetRandom()
 {
+#ifdef HAVE_ARC4RANDOM_FN
+    return arc4random();
+#else
     uint32_t result = 0;
     OCFillRandomMem((uint8_t*) &result, 4);
     return result;
+#endif
 }
 
 uint8_t OCGetRandomByte(void)
 {
-#ifdef HAVE_SRANDOM
+#ifdef HAVE_ARC4RANDOM_FN
+    return arc4random() & 0x00FF;
+#elif HAVE_SRANDOM
     return random() & 0x00FF;
 #else
     return rand() & 0x00FF;
@@ -224,150 +232,4 @@ uint32_t OCGetRandomRange(uint32_t firstBound, uint32_t secondBound)
     }
     result = ((float)OCGetRandom()/((float)(0xFFFFFFFF))*(float)diff) + (float) base;
     return result;
-}
-
-#if defined(__ANDROID__)
-uint8_t parseUuidChar(char c)
-{
-    if (isdigit(c))
-    {
-        return c - '0';
-    }
-    else
-    {
-        return c - 'a' + 10;
-    }
-}
-uint8_t parseUuidPart(const char *c)
-{
-    return (parseUuidChar(c[0])<<4) + parseUuidChar(c[1]);
-}
-#endif
-
-OCRandomUuidResult OCGenerateUuid(uint8_t uuid[UUID_SIZE])
-{
-    if (!uuid)
-    {
-        return RAND_UUID_INVALID_PARAM;
-    }
-#if defined(__ANDROID__)
-    char uuidString[UUID_STRING_SIZE];
-    int8_t ret = OCGenerateUuidString(uuidString);
-
-    if (ret < 0)
-    {
-        return ret;
-    }
-
-    uuid[ 0] = parseUuidPart(&uuidString[0]);
-    uuid[ 1] = parseUuidPart(&uuidString[2]);
-    uuid[ 2] = parseUuidPart(&uuidString[4]);
-    uuid[ 3] = parseUuidPart(&uuidString[6]);
-
-    uuid[ 4] = parseUuidPart(&uuidString[9]);
-    uuid[ 5] = parseUuidPart(&uuidString[11]);
-
-    uuid[ 6] = parseUuidPart(&uuidString[14]);
-    uuid[ 7] = parseUuidPart(&uuidString[16]);
-
-    uuid[ 8] = parseUuidPart(&uuidString[19]);
-    uuid[ 9] = parseUuidPart(&uuidString[21]);
-
-    uuid[10] = parseUuidPart(&uuidString[24]);
-    uuid[11] = parseUuidPart(&uuidString[26]);
-    uuid[12] = parseUuidPart(&uuidString[28]);
-    uuid[13] = parseUuidPart(&uuidString[30]);
-    uuid[14] = parseUuidPart(&uuidString[32]);
-    uuid[15] = parseUuidPart(&uuidString[34]);
-
-    return RAND_UUID_OK;
-#elif defined(HAVE_UUID_UUID_H)
-    // note: uuid_t is typedefed as unsigned char[16] on linux/apple
-    uuid_generate(uuid);
-    return RAND_UUID_OK;
-#else
-    // Fallback for all platforms is filling the array with random data
-    OCFillRandomMem(uuid, UUID_SIZE);
-    return RAND_UUID_OK;
-#endif
-}
-
-OCRandomUuidResult OCGenerateUuidString(char uuidString[UUID_STRING_SIZE])
-{
-    if (!uuidString)
-    {
-        return RAND_UUID_INVALID_PARAM;
-    }
-    /* GAR FIXME: this should be feature tested since any linux has it but not e.g. freebsd */
-#if defined(__ANDROID__)	/* GAR: test for __linux__ instead?  */
-    int32_t fd = open("/proc/sys/kernel/random/uuid", O_RDONLY);
-    if (fd > 0)
-    {
-        ssize_t readResult = read(fd, uuidString, UUID_STRING_SIZE - 1);
-        close(fd);
-        if (readResult < 0)
-        {
-            return RAND_UUID_READ_ERROR;
-        }
-        else if (readResult < UUID_STRING_SIZE - 1)
-        {
-            uuidString[0] = '\0';
-            return RAND_UUID_READ_ERROR;
-        }
-
-        uuidString[UUID_STRING_SIZE - 1] = '\0';
-        for (char* p = uuidString; *p; ++p)
-        {
-            *p = tolower(*p);
-        }
-        return RAND_UUID_OK;
-    }
-    else
-    {
-        close(fd);
-        return RAND_UUID_READ_ERROR;
-    }
-#elif defined(HAVE_UUID_UUID_H)
-    uint8_t uuid[UUID_SIZE];
-    int8_t ret = OCGenerateUuid(uuid);
-
-    if (ret != 0)
-    {
-        return ret;
-    }
-
-    uuid_unparse_lower(uuid, uuidString);
-    return RAND_UUID_OK;
-
-#else
-    uint8_t uuid[UUID_SIZE];
-    OCGenerateUuid(uuid);
-
-    return OCConvertUuidToString(uuid, uuidString);
-#endif
-}
-
-OCRandomUuidResult OCConvertUuidToString(const uint8_t uuid[UUID_SIZE],
-                                         char uuidString[UUID_STRING_SIZE])
-{
-    if (uuid == NULL || uuidString == NULL)
-    {
-        return RAND_UUID_INVALID_PARAM;
-    }
-
-
-    int ret = snprintf(uuidString, UUID_STRING_SIZE,
-            "%02x%02x%02x%02x-%02x%02x-%02x%02x-%02x%02x-%02x%02x%02x%02x%02x%02x",
-            uuid[0], uuid[1], uuid[2], uuid[3],
-            uuid[4], uuid[5], uuid[6], uuid[7],
-            uuid[8], uuid[9], uuid[10], uuid[11],
-            uuid[12], uuid[13], uuid[14], uuid[15]
-            );
-
-    if (ret != UUID_STRING_SIZE - 1)
-    {
-        return RAND_UUID_CONVERT_ERROR;
-    }
-
-    return RAND_UUID_OK;
 }
