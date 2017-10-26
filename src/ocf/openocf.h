3abeb08e6c99e536a51a37b2291f4d6c2d93fd93
Gregg Reynolds <reynolds-gregg@norc.org>
2017-10-25 23:24:21 -0500

    add openocf.h as public header
    * use bazel's include transitivity to put all public header in openocf.h
    
    Signed-off-by: Gregg Reynolds <reynolds-gregg@norc.org>

---
+++ src/ocf/openocf.h
@@ -0,0 +1,15 @@
+#ifndef OPENOCF_H
+#define OPENOCF_H
+
+
+#include "iotivity_config.h"
+#include "ocstack.h"
+#include "logger.h"
+#include "cacommon.h"
+#include "cJSON.h"
+#include "coap/pdu.h"
+
+#include "client/co_service_provider_mgr.h"
+
+
+#endif /* OPENOCF_H */
