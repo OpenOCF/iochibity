7055fea6740e0ebf0270a76b7bf8153079290c7a
Gregg Reynolds <reynolds-gregg@norc.org>
2017-11-28 00:48:49 -0600

    git the coap includes right
    Signed-off-by: Gregg Reynolds <reynolds-gregg@norc.org>

--- src/comm/caprotocolmessage.c
+++ src/comm/caprotocolmessage.c
@@ -43,6 +43,15 @@
 #define TAG "OIC_CA_PRTCL_MSG"
 
 #if INTERFACE
+#include "coap_config.h"
+#include "coap/block.h"
+#include "coap/coap_list.h"
+#include "coap/mem.h"
+#include "coap/pdu.h"
+#include "coap/prng.h"
+#endif
+
+#if INTERFACE
 /**
  * Message Type for Base source code.
  */
