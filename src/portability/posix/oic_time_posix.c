1501651d3d506ee054764b55e4669220253dad38
Gregg Reynolds <reynolds-gregg@norc.org>
2017-11-28 03:35:13 -0600

    remove duplicate code
    Signed-off-by: Gregg Reynolds <reynolds-gregg@norc.org>

--- src/portability/posix/oic_time_posix.c
+++ src/portability/posix/oic_time_posix.c
@@ -74,63 +74,63 @@ typedef enum
 } OICTimePrecision;
 #endif	/* EXPORT_INTERFACE */
 
-uint64_t OICGetCurrentTime(OICTimePrecision precision)
-{
-    uint64_t currentTime = 0;
-
-#ifdef _WIN32
-    static LARGE_INTEGER frequency = {0};
-
-    if (!frequency.QuadPart)
-    {
-        QueryPerformanceFrequency(&frequency);
-    }
-
-    LARGE_INTEGER count = {0};
-    QueryPerformanceCounter(&count);
-
-    currentTime =
-    (TIME_IN_MS == precision)
-        ? count.QuadPart / (frequency.QuadPart / MS_PER_SEC)
-        : count.QuadPart / (frequency.QuadPart / US_PER_SEC);
-#else
-# if _POSIX_TIMERS > 0
-#   if defined(CLOCK_MONOTONIC_COARSE)
-    static const clockid_t clockId = CLOCK_MONOTONIC_COARSE;
-#   elif _POSIX_MONOTONIC_CLOCK >= 0
-    // Option _POSIX_MONOTONIC_CLOCK == 0 indicates that the option is
-    // available at compile time but may not be supported at run
-    // time.  Check if option _POSIX_MONOTONIC_CLOCK is supported at
-    // run time.
-#     if _POSIX_MONOTONIC_CLOCK == 0
-    static const clockid_t clockId =
-        sysconf(_SC_MONOTONIC_CLOCK) > 0 ? CLOCK_MONOTONIC : CLOCK_REALTIME;
-#     else
-    static const clockid_t clockId = CLOCK_MONOTONIC;
-#     endif  // _POSIX_MONOTONIC_CLOCK == 0
-#   else
-    static const clockid_t clockId = CLOCK_REALTIME;
-#   endif  // CLOCK_MONOTONIC_COARSE
-
-    struct timespec current = { .tv_sec = 0, .tv_nsec = 0 };
-    if (clock_gettime(clockId, &current) == 0)
-    {
-        currentTime =
-            (TIME_IN_MS == precision)
-            ? (((uint64_t) current.tv_sec * MS_PER_SEC) + (current.tv_nsec / NS_PER_MS))
-            : (((uint64_t) current.tv_sec * US_PER_SEC) + (current.tv_nsec / NS_PER_US));
-    }
-# else
-    struct timeval current = { .tv_sec = 0, .tv_usec = 0 };
-    if (gettimeofday(&current, NULL) == 0)
-    {
-        currentTime =
-            (TIME_IN_MS == precision)
-            ? (((uint64_t) current.tv_sec * MS_PER_SEC) + (current.tv_usec / US_PER_MS))
-            : (((uint64_t) current.tv_sec * US_PER_SEC) + (current.tv_usec));
-    }
-# endif  // _POSIX_TIMERS > 0
-#endif  // _WIN32
-
-    return currentTime;
-}
+/* uint64_t OICGetCurrentTime(OICTimePrecision precision)
+ * {
+ *     uint64_t currentTime = 0;
+ * 
+ * #ifdef _WIN32
+ *     static LARGE_INTEGER frequency = {0};
+ * 
+ *     if (!frequency.QuadPart)
+ *     {
+ *         QueryPerformanceFrequency(&frequency);
+ *     }
+ * 
+ *     LARGE_INTEGER count = {0};
+ *     QueryPerformanceCounter(&count);
+ * 
+ *     currentTime =
+ *     (TIME_IN_MS == precision)
+ *         ? count.QuadPart / (frequency.QuadPart / MS_PER_SEC)
+ *         : count.QuadPart / (frequency.QuadPart / US_PER_SEC);
+ * #else
+ * # if _POSIX_TIMERS > 0
+ * #   if defined(CLOCK_MONOTONIC_COARSE)
+ *     static const clockid_t clockId = CLOCK_MONOTONIC_COARSE;
+ * #   elif _POSIX_MONOTONIC_CLOCK >= 0
+ *     // Option _POSIX_MONOTONIC_CLOCK == 0 indicates that the option is
+ *     // available at compile time but may not be supported at run
+ *     // time.  Check if option _POSIX_MONOTONIC_CLOCK is supported at
+ *     // run time.
+ * #     if _POSIX_MONOTONIC_CLOCK == 0
+ *     static const clockid_t clockId =
+ *         sysconf(_SC_MONOTONIC_CLOCK) > 0 ? CLOCK_MONOTONIC : CLOCK_REALTIME;
+ * #     else
+ *     static const clockid_t clockId = CLOCK_MONOTONIC;
+ * #     endif  // _POSIX_MONOTONIC_CLOCK == 0
+ * #   else
+ *     static const clockid_t clockId = CLOCK_REALTIME;
+ * #   endif  // CLOCK_MONOTONIC_COARSE
+ * 
+ *     struct timespec current = { .tv_sec = 0, .tv_nsec = 0 };
+ *     if (clock_gettime(clockId, &current) == 0)
+ *     {
+ *         currentTime =
+ *             (TIME_IN_MS == precision)
+ *             ? (((uint64_t) current.tv_sec * MS_PER_SEC) + (current.tv_nsec / NS_PER_MS))
+ *             : (((uint64_t) current.tv_sec * US_PER_SEC) + (current.tv_nsec / NS_PER_US));
+ *     }
+ * # else
+ *     struct timeval current = { .tv_sec = 0, .tv_usec = 0 };
+ *     if (gettimeofday(&current, NULL) == 0)
+ *     {
+ *         currentTime =
+ *             (TIME_IN_MS == precision)
+ *             ? (((uint64_t) current.tv_sec * MS_PER_SEC) + (current.tv_usec / US_PER_MS))
+ *             : (((uint64_t) current.tv_sec * US_PER_SEC) + (current.tv_usec));
+ *     }
+ * # endif  // _POSIX_TIMERS > 0
+ * #endif  // _WIN32
+ * 
+ *     return currentTime;
+ * } */
