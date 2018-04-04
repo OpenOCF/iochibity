ea16060327064e04b2083cd2559b3a0afed3f51d
Gregg Reynolds <reynolds-gregg@norc.org>
2018-04-04 11:58:49 -0500

    add autoconf shell script for rpi3b cross-config
    Signed-off-by: Gregg Reynolds <reynolds-gregg@norc.org>

---
+++ bin/config_rpi.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+set -x
+
+export PATH="${PATH}:/Volumes/CrossToolNG/armv8-rpi3-linux-gnueabihf/bin"
+
+export COSYSROOT=$HOME/cosysroots/rpi3b
+
+echo COSYSROOT: $COSYSROOT
+
+./configure --build=x86_64-apple-darwin \
+            --host=armv8-rpi3-linux-gnueabihf \
+            --enable-kernel=4.9.35 \
+            --prefix=/usr \
+	    --mandir="/usr/share/man" \
+	    --without-manpages \
+	    --with-shared \
+	    --libdir="/usr/lib/arm-linux-gnueabihf" \
+	    --disable-examples \
+            CPPFLAGS="-P"
+
+
+	    # --with-terminfo-dirs="/etc/terminfo:/lib/terminfo:/usr/share/terminfo" \
+	    # --with-default-terminfo-dir="/etc/terminfo" \
