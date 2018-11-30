89d0754abbb2e5bdfee126a13225939078d25f90
Gregg Reynolds <reynolds-gregg@norc.org>
2018-11-30 10:16:02 -0600

    remove macros (moved to rolesresource.c)
    Signed-off-by: Gregg Reynolds <reynolds-gregg@norc.org>

--- src/sec/_misc.h
+++ src/sec/_misc.h
@@ -20,9 +20,6 @@ typedef struct OicUuid OicUuid_t; //TODO is UUID type defined elsewhere?
  * /oic/uuid (Universal Unique Identifier) data type.
  */
 /* #define UUID_LENGTH 128/8 // 128-bit GUID length - ca_adapter_net_ssl.c */
-//TODO: Confirm the length and type of ROLEID.
-#define ROLEID_LENGTH 64 // 64-byte authority max length
-#define ROLEAUTHORITY_LENGTH 64 // 64-byte authority max length
 #define OWNER_PSK_LENGTH_128 128/8 //byte size of 128-bit key size
 #define OWNER_PSK_LENGTH_256 256/8 //byte size of 256-bit key size
 
