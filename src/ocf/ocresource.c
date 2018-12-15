7b3cf80d1c11a525bf1b53491dedb6eb3a1c5707
Gregg Reynolds <reynolds-gregg@norc.org>
2018-12-15 11:54:26 -0600

    mv device code from ocresource.c to oocf_device.c
    Signed-off-by: Gregg Reynolds <reynolds-gregg@norc.org>

--- src/ocf/ocresource.c
+++ src/ocf/ocresource.c
@@ -879,92 +879,12 @@ OCStackResult BuildResponseRepresentation(const OCResource *resourcePtr,
     return OC_STACK_OK;
 }
 
-void CleanUpDeviceProperties(OCDeviceProperties **deviceProperties)
-{
-    if (!deviceProperties || !(*deviceProperties))
-    {
-        return;
-    }
+/* CleanUpDeviceProperties => oocf_device.c */
 
-    OICFreeAndSetToNull((void**)(deviceProperties));
-}
+/* CreateDeviceProperties => oocf_device.c */
 
-static OCStackResult CreateDeviceProperties(const char *piid, OCDeviceProperties **deviceProperties)
-{
-    OIC_LOG(DEBUG, TAG, "CreateDeviceProperties IN");
+/* GenerateDeviceProperties => oocf_device.c */
 
-    OCStackResult result = OC_STACK_OK;
-
-    if (!piid || !deviceProperties)
-    {
-        return OC_STACK_INVALID_PARAM;
-    }
-
-    *deviceProperties = (OCDeviceProperties*)OICCalloc(1, sizeof(OCDeviceProperties));
-    if (*deviceProperties)
-    {
-        OICStrcpy((*deviceProperties)->protocolIndependentId, UUID_STRING_SIZE, piid);
-    }
-    else
-    {
-        result = OC_STACK_NO_MEMORY;
-    }
-
-    OIC_LOG(DEBUG, TAG, "CreateDeviceProperties OUT");
-
-    return result;
-}
-
-static OCStackResult GenerateDeviceProperties(OCDeviceProperties **deviceProperties)
-{
-    OCStackResult result = OC_STACK_OK;
-    OicUuid_t generatedProtocolIndependentId = {.id = {0}};
-    char* protocolIndependentId = NULL;
-
-    if (!deviceProperties)
-    {
-        return OC_STACK_INVALID_PARAM;
-    }
-
-    *deviceProperties = NULL;
-
-    // Generate a UUID for the Protocol Independent ID
-    if (OCGenerateUuid(generatedProtocolIndependentId.id))
-    {
-        protocolIndependentId = (char*)OICCalloc(UUID_STRING_SIZE, sizeof(char));
-        if (protocolIndependentId)
-        {
-            if (!OCConvertUuidToString(generatedProtocolIndependentId.id, protocolIndependentId))
-            {
-                OIC_LOG(ERROR, TAG, "ConvertUuidToStr failed");
-                result = OC_STACK_ERROR;
-            }
-        }
-        else
-        {
-            result = OC_STACK_NO_MEMORY;
-        }
-    }
-    else
-    {
-        OIC_LOG(FATAL, TAG, "Generate UUID for Device Properties Protocol Independent ID failed!");
-        result = OC_STACK_ERROR;
-    }
-
-    if (OC_STACK_OK == result)
-    {
-        result = CreateDeviceProperties(protocolIndependentId, deviceProperties);
-        if (OC_STACK_OK != result)
-        {
-            OIC_LOG(ERROR, TAG, "CreateDeviceProperties failed");
-        }
-    }
-
-    // Clean Up
-    OICFreeAndSetToNull((void**)&protocolIndependentId);
-
-    return result;
-}
 
 OCStackResult CBORPayloadToDeviceProperties(const uint8_t *payload, size_t size, OCDeviceProperties **deviceProperties)
 {
