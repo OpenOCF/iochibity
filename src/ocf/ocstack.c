b194a99d79bed12b4aaec499730bfcc96e6d36b7
Gregg Reynolds <reynolds-gregg@norc.org>
2018-12-26 16:06:41 -0600

    mv zone id stuff out of ocstack to ocendpoint
    Signed-off-by: Gregg Reynolds <reynolds-gregg@norc.org>

--- src/ocf/ocstack.c
+++ src/ocf/ocstack.c
@@ -926,68 +926,6 @@ OCStackResult HandleBatchResponse(char *requestUri, OCRepPayload **payload)
     return OC_STACK_INVALID_PARAM;
 }
 
-/**
- * Map zoneId to endpoint address which scope is ipv6 link-local.
- * @param payload Discovery payload which has Endpoint information.
- * @param ifindex index which indicate network interface.
- * => oocf_ipv6.c?
- * FIXME: name it accurately
- */
-#if defined (IP_ADAPTER)
-OCStackResult OCMapZoneIdToLinkLocalEndpoint(OCDiscoveryPayload *payload, uint32_t ifindex)
-{
-    if (!payload)
-    {
-        OIC_LOG(ERROR, TAG, "Given argument payload is NULL!!");
-        return OC_STACK_INVALID_PARAM;
-    }
-
-    OCResourcePayload *curRes = payload->resources;
-
-    while (curRes != NULL)
-    {
-        OCEndpointPayload* eps = curRes->eps;
-
-        while (eps != NULL)
-        {
-            if (eps->family & OC_IP_USE_V6)
-            {
-                CATransportFlags_t scopeLevel;
-                if (CA_STATUS_OK == CAGetIpv6AddrScope(eps->addr, &scopeLevel))
-                {
-                    if (CA_SCOPE_LINK == scopeLevel)
-                    {
-                        char *zoneId = NULL;
-                        if (OC_STACK_OK == OCGetLinkLocalZoneId(ifindex, &zoneId))
-                        {
-                            assert(zoneId != NULL);
-                            // put zoneId to end of addr
-                            OICStrcat(eps->addr, OC_MAX_ADDR_STR_SIZE, "%25");
-                            OICStrcat(eps->addr, OC_MAX_ADDR_STR_SIZE, zoneId);
-                            OICFree(zoneId);
-                        }
-                        else
-                        {
-                            OIC_LOG(ERROR, TAG, "failed at parse zone-id for link-local address");
-                            return OC_STACK_ERROR;
-                        }
-                    }
-                }
-                else
-                {
-                    OIC_LOG(ERROR, TAG, "failed at parse ipv6 scope level");
-                    return OC_STACK_ERROR;
-                }
-            }
-            eps = eps->next;
-        }
-        curRes = curRes->next;
-    }
-
-    return OC_STACK_OK;
-}
-#endif
-
 /* OCHandleResponse => oocf_client.c */
 
 /* HandleCAResponses => oocf_client.c */
