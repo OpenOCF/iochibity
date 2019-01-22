900aedc636a4af457f11029b9e6d569bd9fb1e44
Gregg Reynolds <reynolds-gregg@norc.org>
2019-01-22 11:22:01 -0600

    add jni CoAPOption.c
    * support creation of java CoAPOption objects from jni
    
    Signed-off-by: Gregg Reynolds <reynolds-gregg@norc.org>

---
+++ bindings/javalang/pojo/jni/openocf_message_CoAPOption.ids.h
@@ -0,0 +1,10 @@
+#ifndef openocf_message_CoAPOption_ids_h
+#define openocf_message_CoAPOption_ids_h
+
+#include <jni.h>
+
+extern jclass    K_COAP_OPTION;
+extern jmethodID MID_COAP_OPTION_CTOR;
+
+#endif
+
